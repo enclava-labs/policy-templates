d3898f7dc0fe73e1dd219f13162647b95d582e58
Aljaz Ceru <aljaz@ceru.si>
2026-05-08 15:00:13 +0200

    Allow caddy rootfs bind target in policy

--- signing-service/src/genpolicy.rs
+++ signing-service/src/genpolicy.rs
@@ -809,7 +809,7 @@ fn tenant_ingress_container(descriptor: &DeploymentDescriptor) -> Value {
             mount("tenant-ingress-caddyfile", "/etc/caddy", true),
             mount("unlock-socket", "/run/enclava", false),
         ],
-        "securityContext": security_context(10002, 10002, true, false, false, caps(&["ALL"], &["NET_BIND_SERVICE"])),
+        "securityContext": security_context(10002, 10002, false, false, false, caps(&["ALL"], &["NET_BIND_SERVICE"])),
         "resources": resources("100m", "128Mi", "500m", "256Mi"),
     })
 }
